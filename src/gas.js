de0b8d47964e5ba12af736203a767ce404a985e9
Michael <hellomikko@outlook.com>
2023-02-08 01:33:11 -0500

    update 1 file and delete 1 file

--- src/gas.js
+++ src/gas.js
@@ -9,8 +9,11 @@ if (require('electron-squirrel-startup')) {
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 860,
+    height: 680,
+    frame: true,
+    resizable: true,
+    autoHideMenuBar: true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
